ad7cc5dc751d046f4312e13b1475e2cae4c4328a
lklegal <110703055+lklegal@users.noreply.github.com>
2023-05-23 17:55:06 -0300

    sequencial.c consertado

--- Concorrente - Projeto/sequencial.c
+++ Concorrente - Projeto/sequencial.c
@@ -1,17 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int *gerar_vetor_inteiro(int n);
+
 int main(int argc, char** argv) {
 
-    int tam = 10;
-
-    int arr[] = {8, 4, 5, 2, 9, 10, 3, 1, 6, 7};
-    int count[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int tam = 10000;
+    int count[tam];
+    int *arr;
 
     int sortedArr[tam];
+    
+    arr = gerar_vetor_inteiro(tam);
+
+    for (int i = 0; i < tam; i++){
+        count[i] = 0;
+    }
+
+    for (int i = 0; i < tam; i++){
+        sortedArr[i] = 0;
+    }
 
     for(int i = 0; i < tam; i++){
-
         for (int j = 0; j < tam; j++){
             if(arr[i] > arr[j]){
 
@@ -23,16 +33,48 @@ int main(int argc, char** argv) {
     for (int i = 0; i < tam; i++){
         for (int j = 0; j < tam; j++)
         {
-
             if(count[i] == j){
-
                 sortedArr[j] = arr[i];
             }
         }
     }
-    
+
+    int ultimoNaoZero = 0;
+
+    for(int i = 0; i < tam; i++){
+        if (sortedArr[i] != 0)
+        {
+            ultimoNaoZero = sortedArr[i];
+        }else
+        {
+            sortedArr[i] = ultimoNaoZero;
+        }
+        
+        
+    }
+
+/*
+    for (int i = 0; i < tam; i++)
+    {
+        printf("%d,", arr[i]);
+    }
+    printf("\n");
     for (int i = 0; i < tam; i++)
     {
         printf("%d, ", sortedArr[i]);
     }
+*/
+}
+
+//1 2 3 3 5 5 6 6 7 9 
+
+int *gerar_vetor_inteiro(int n) {
+    int *vetor;
+    int i;
+    vetor = (int *)malloc(sizeof(int) * n);
+    for (i=0;i<n;i++) {
+        int num = (rand() % 10000);
+        vetor[i] = num;
+    }
+    return vetor;
 }
